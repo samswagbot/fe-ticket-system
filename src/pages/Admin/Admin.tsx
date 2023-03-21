1e7013fd63e0b39eb8386558826798fea2b627b0
Samson <sammossallam@gmail.com>
2023-03-21 16:55:48 -0400

    Admin page

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,43 @@
+import { CircularProgress, Stack } from "@mui/material";
+import { useEffect, useState } from "react";
+import TicketListItem from "../../components/TicketListItem/TicketListItem";
+import { Ticket } from "../../types/ticket";
+import styles from "../container.module.css";
+
+export default function Admin() {
+  const [tickets, setTickets] = useState<Ticket[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const getTickets = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch("/api/tickets");
+      const data = await res.json();
+      setTickets(data);
+    } catch (error) {
+      setError("Can not fetch tickets");
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getTickets();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Stack spacing={3}>
+        {!loading && tickets ? (
+          tickets.map((ticket, index) => (
+            <TicketListItem key={index} ticket={ticket} index={index} />
+          ))
+        ) : (
+          <CircularProgress />
+        )}
+      </Stack>
+      {error && <div>{error}</div>}
+    </div>
+  );
+}
