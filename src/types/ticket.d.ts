6990a5a76672a5c2c2983e1d55b10fd1166497c4
Samson <sammossallam@gmail.com>
2023-03-21 16:55:29 -0400

    tickets types

---
+++ src/types/ticket.d.ts
@@ -0,0 +1,9 @@
+export interface Ticket {
+    _id: string;
+    name: string;
+    response: string;
+    email: string;
+    description: string;
+    status: "in progress" | "resolved" | "new";
+  }
+  
