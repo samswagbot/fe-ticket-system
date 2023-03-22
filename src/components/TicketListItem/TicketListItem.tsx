9f2370108157c8c878f150899e9cc82afcccd0f3
Samson <sammossallam@gmail.com>
2023-03-22 12:50:41 -0400

    style fixes for container and ticket

--- src/components/TicketListItem/TicketListItem.tsx
+++ src/components/TicketListItem/TicketListItem.tsx
@@ -1,42 +1,39 @@
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { Ticket } from "../../types/ticket";
 import styles from "./ticket.module.css";
 
-export default function TicketListItem({
-  ticket,
-  index,
-}: {
-  ticket: Ticket;
-  index: number;
-}) {
-  const { description, name, status, email } = ticket;
+export default function TicketListItem({ ticket }: { ticket: Ticket }) {
+  const { description, name, status, email, _id } = ticket;
+  const navigate = useNavigate();
   return (
     <div className={styles.ticket}>
-      <h1 className={styles.title}>Ticket {index + 1}</h1>
+      <h2 className={styles.title}>Ticket: {_id}</h2>
       <div className={styles.content}>
         <div className={styles.details}>
-          <div>
-            <p>
-              <strong>Name: </strong>
-              <span>{name}</span>
-            </p>
-            <p>
-              <strong>Issue Description: </strong>
-              <span>{description}</span>
-            </p>
-          </div>
-          <div>
-            <p>
-              <strong>Status: </strong>
-              <span>{status}</span>
-            </p>
-            <p>
-              <strong>Email: </strong>
-              <span>{email}</span>
-            </p>
-          </div>
+          <p>
+            <strong>Name: </strong>
+            <span>{name}</span>
+          </p>
+
+          <p>
+            <strong>Status: </strong>
+            <span>{status}</span>
+          </p>
+          <p>
+            <strong>Email: </strong>
+            <span>{email}</span>
+          </p>
+          <p className={styles.desc}>
+            <strong>Issue Description: </strong>
+            <span>{description}</span>
+          </p>
         </div>
-        <Button fullWidth variant="contained">
+        <Button
+          onClick={() => navigate(`/admin/${_id}`)}
+          fullWidth
+          variant="contained"
+        >
           See more
         </Button>
       </div>
